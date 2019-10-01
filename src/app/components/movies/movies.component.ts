ad2f869a069d338e586649723660be423c71fdb3
Arthur Boron <aboron@oxiane.com>
2019-10-01 08:50:56 +0200

    filter movies by genre working

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterContentChecked, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterContentChecked, OnDestroy, OnChanges } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -9,19 +9,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MoviesComponent implements OnInit {
   trendingMovies: any;
-  filteredTrendingMovies: any;
+  genres: any;
 
-  constructor(private activeRoute: ActivatedRoute, private movieService: MovieService) {
-    this.movieService.getTrendingMovies(1).subscribe(data => {
+  constructor(private activeRoute: ActivatedRoute, private movieService: MovieService, private router: Router) {
+      this.movieService.getTrendingMovies(1, -1).subscribe(data => {
       this.trendingMovies = data['results'];
-    });
+      });
+      this.movieService.getGenres().subscribe(data => {
+      this.genres = data['genres'];
+      });
   }
 
   ngOnInit() {
     this.activeRoute.queryParams.subscribe(queryParams => {
-      this.movieService.getTrendingMovies(queryParams.page).subscribe(data => {
-        this.trendingMovies = data['results'];
+      this.movieService.getGenres().subscribe(data => {
+        const genreName = this.getNameOfGenreDependingOnUrl();
+        let genreId = -1;
+        genreId = this.getIdOfGenreDependingOnUrl(data['genres'], genreName);
+        this.movieService.getTrendingMovies(queryParams.page, genreId).subscribe(datas => {
+          this.trendingMovies = datas['results'];
+        });
       });
     });
+  }
+
+  routeGenre() {
+    const selectElem = document.getElementsByName('my_html_select_box');
+    const selectElemTest = selectElem[0].selectedIndex;
+    let valueOfSelected = selectElem[0][selectElemTest].innerHTML;
+    if (valueOfSelected.endsWith(' ')) {
+      valueOfSelected = valueOfSelected.substring(0, valueOfSelected.length - 1);
+    }
+    this.activeRoute.queryParams.subscribe(queryParams => {
+      this.router.navigate(['/movie'], { queryParams: { page: 1, genre: valueOfSelected }});
+    });
+  }
+
+  getNameOfGenreDependingOnUrl() {
+    const parameters = new URLSearchParams(window.location.search);
+    const stringGenre = parameters.get('genre');
+    return stringGenre;
+  }
+
+  getIdOfGenreDependingOnUrl(genres, genreName: string) {
+    let id = -1;
+    genres.forEach(element => {
+      if (genreName === element.name) {
+        id =  element.id;
+      }
+    });
+    return id;
+  }
+
+  goTop() {
+    window.scrollTo(0, 0);
   }
 }
