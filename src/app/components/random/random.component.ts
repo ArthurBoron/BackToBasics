6d433cb150101ae868923795d37d9822d5bcd5cc
Aymeric <barou@et.esiea.fr>
2019-10-01 12:46:22 +0530

    update random and css of the header

--- src/app/components/random/random.component.ts
+++ src/app/components/random/random.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-random',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomComponent implements OnInit {
 
-  constructor() { }
+  private movieUrl = 'https://api.themoviedb.org/3/';
+  private apiKey = '52f8b1f1fd9b853d910f3fb53654d48c';
+
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  closeAd(){
+    document.getElementById('close').style.height = '0%';
+  }
+
+  openAd(){
+    document.getElementById('close').style.height = '61%';
+  }
+
+  randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getMovie(id: number) {
+    return this.http.get(`${this.movieUrl}movie/${id}?api_key=${this.apiKey}&language=en-US`);
+  }
+
+
 }
+
